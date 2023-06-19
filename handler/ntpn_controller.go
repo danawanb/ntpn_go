31fa7df514b558280ce32553483c9ef2ff53fdf2
danawanb <danawanbimantoro23@gmail.com>
2023-06-20 00:19:19 +0700

    update templating

--- handler/ntpn_controller.go
+++ handler/ntpn_controller.go
@@ -236,6 +236,7 @@ func GetTokenUsingGetRequest() bool {
 	cookie_token, err := GetToken("token_ntpn_bulk")
 	if err != nil {
 		log.Println(err.Error() + "tidak bisa mengambil token")
+		return false
 	}
 
 	req.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
